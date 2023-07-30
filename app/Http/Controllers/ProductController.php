68ef210de04965c65e8cf543ae68b2ef26e3cca9
Hassan Moussa <hassanmoussa61@gmail.com>
2023-07-30 16:36:47 +0300

    added delete route and function in controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 
+
 class ProductController extends Controller
 {
     public function getProducts()
@@ -13,4 +14,21 @@ class ProductController extends Controller
 
         return response()->json($products);
     }
+
+
+    public function deleteProduct($id)
+    {
+        try {
+
+            $product = Product::find($id);
+            $product->delete();
+
+            return response()->json(['message' => 'Product deleted successfully ' . $id], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to delete product'], 500);
+        }
+
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,4 @@ Route::controller(AuthController::class)->group(function () {
 
 
 Route::get('/products', [ProductController::class, 'getProducts']);
+Route::delete('/products/delete/{id}', [ProductController::class, 'deleteProduct']);
